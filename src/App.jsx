e333aa04c9865a60fc96a63a1b830c79bea536b5
AqibNiazi <aqibjaved5201@gmail.com>
2025-02-18 14:02:36 +0500

    demo web-app completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const handleClick = (selectedButton) => {
     setSelectedContent(selectedButton);
   };
-  console.log("Examples", EXAMPLES);
+
   return (
     <div>
       <Header />
@@ -16,25 +16,38 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS?.[0]?.title}
-              description={CORE_CONCEPTS?.[0]?.description}
-              image={CORE_CONCEPTS?.[0]?.image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS?.map((conceptItem) => (
+              <CoreConcept key={conceptItem?.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={() => handleClick("components")}>
+            <TabButton
+              isSelected={selectedContent == "components"}
+              onClick={() => handleClick("components")}
+            >
               Components
             </TabButton>
-            <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
-            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
-            <TabButton onClick={() => handleClick("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedContent == "jsx"}
+              onClick={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedContent == "props"}
+              onClick={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedContent == "state"}
+              onClick={() => handleClick("state")}
+            >
+              State
+            </TabButton>
           </menu>
           {selectedContent ? (
             <div id="tab-content">
